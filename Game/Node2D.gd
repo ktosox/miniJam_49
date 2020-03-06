fdb9afaf982d406b42ecbcf98183cfe6320c9f40
ktosox <ktoso3@gmail.com>
2020-03-06 18:55:13 +0100

    creating project, drfating assets

---
+++ Game/Node2D.gd
@@ -0,0 +1,50 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+export var timeShift = 1.0
+var shiftJuice = 200
+export var isShifting = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	Engine.time_scale = timeShift
+
+	pass
+
+
+func _on_Down_toggled(button_pressed):
+	print("derp")
+	if(button_pressed):
+		timeShift = 0.5
+		isShifting = true
+		$CanvasLayer/TimeShifter.playback_speed = 2
+		$CanvasLayer/TimeShifter.play("Down")
+	else:
+		$CanvasLayer/TimeShifter.stop()
+		isShifting = false
+		timeShift = 1
+		$CanvasLayer/CPUParticles2D.gravity = Vector2()
+	pass # Replace with function body.
+
+
+func _on_Up_toggled(button_pressed):
+	if(button_pressed):
+		timeShift = 2
+		isShifting = true
+		$CanvasLayer/TimeShifter.playback_speed = 0.5
+		$CanvasLayer/TimeShifter.play("Up")
+	else:
+		$CanvasLayer/TimeShifter.stop()
+		isShifting = false
+		timeShift = 1
+		$CanvasLayer/CPUParticles2D.gravity = Vector2()
+	pass # Replace with function body.
---
+++ Game/Node2D2.gd
@@ -0,0 +1,37 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	for i in 4:
+		var copy = $TileMap.duplicate()
+		
+		var layer = CanvasLayer.new()
+		layer.follow_viewport_enable = true
+		layer.follow_viewport_scale = 1 - (i)*0.04
+		layer.layer =-i
+		#layer.scale *= layer.follow_viewport_scale
+		print(layer.follow_viewport_scale)
+		copy.modulate*=layer.follow_viewport_scale-(i*0.2)
+		add_child(layer)
+		layer.add_child(copy)
+	$TileMap.visible=false
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if(Input.is_action_pressed("ui_left")):
+		$Camera2D.position.x-=delta*600
+	if(Input.is_action_pressed("ui_right")):
+		$Camera2D.position.x+=delta*600
+	if(Input.is_action_pressed("ui_up")):
+		$Camera2D.position.y-=delta*600
+	if(Input.is_action_pressed("ui_down")):
+		$Camera2D.position.y+=delta*600
+	pass
